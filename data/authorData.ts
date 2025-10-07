a135869a432569de414d7c87cc840fa646875a4f
Jayden <jkohhokj@gmail.com>
2025-10-07 16:03:45 -0500

    update resume

--- data/authorData.ts
+++ data/authorData.ts
@@ -10,7 +10,7 @@ export const authorData: { [key: string]: string } =
         mail: 'jkohhokj@gmail.com',
         github: 'https://www.github.com/jkohhokj',
         linkedin: 'https://www.linkedin.com/in/jkohhokj',
-        resume: '/files/Resume_2025_7.pdf',
+        resume: '/files/Resume_2025_8.pdf',
         source: 'https://www.github.com/jkohhokj/website',
         blog: 'https://blog.jkoh.dev'
 
